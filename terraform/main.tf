e682d6b64fc7e70659b73f369bf762c534053835
Arslan Ametov <arsinux@yandex.com>
2023-11-13 17:58:53 +0500

    terraform applied

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,14 +21,14 @@ resource "yandex_vpc_subnet" "foo" {
   v4_cidr_blocks = ["10.5.0.0/24"]
 }
 
-resource "yandex_container_registry" "registry1" {
-  name = "registry1"
+resource "yandex_container_registry" "arsinux" {
+  name = "arsinux"
 }
 
 locals {
-  folder_id = "<INSERT YOUR FOLDER ID>"
+  folder_id = "b1gchsq9rppdno9li997"
   service-accounts = toset([
-    "catgpt-sa",
+    "catgpt-saa",
   ])
   catgpt-sa-roles = toset([
     "container-registry.images.puller",
@@ -42,7 +42,7 @@ resource "yandex_iam_service_account" "service-accounts" {
 resource "yandex_resourcemanager_folder_iam_member" "catgpt-roles" {
   for_each  = local.catgpt-sa-roles
   folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-sa"].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-saa"].id}"
   role      = each.key
 }
 
@@ -51,7 +51,7 @@ data "yandex_compute_image" "coi" {
 }
 resource "yandex_compute_instance" "catgpt-1" {
     platform_id        = "standard-v2"
-    service_account_id = yandex_iam_service_account.service-accounts["catgpt-sa"].id
+    service_account_id = yandex_iam_service_account.service-accounts["catgpt-saa"].id
     resources {
       cores         = 2
       memory        = 1
@@ -73,8 +73,81 @@ resource "yandex_compute_instance" "catgpt-1" {
     }
     metadata = {
       docker-compose = file("${path.module}/docker-compose.yaml")
-      ssh-keys  = "ubuntu:${file("~/.ssh/devops_training.pub")}"
+      ssh-keys  = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
     }
 }
 
+resource "yandex_compute_instance" "catgpt-2" {
+    platform_id        = "standard-v2"
+    service_account_id = yandex_iam_service_account.service-accounts["catgpt-saa"].id
+    resources {
+      cores         = 2
+      memory        = 1
+      core_fraction = 5
+    }
+    scheduling_policy {
+      preemptible = true
+    }
+    network_interface {
+      subnet_id = "${yandex_vpc_subnet.foo.id}"
+      nat = true
+    }
+    boot_disk {
+      initialize_params {
+        type = "network-hdd"
+        size = "30"
+        image_id = data.yandex_compute_image.coi.id
+      }
+    }
+    metadata = {
+      docker-compose = file("${path.module}/docker-compose.yaml")
+      ssh-keys  = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    }
+}
+
+resource "yandex_lb_network_load_balancer" "my_lb" {
+  name = "my-load-balancer"
+
+  listener {
+    name        = "app"
+    port        = 8080
+    target_port = 8080
+    protocol    = "tcp"
+  }
+  listener {
+    name        = "metrics"
+    port        = 9090
+    target_port = 9090
+    protocol    = "tcp"
+  }
+
+  attached_target_group {
+    target_group_id = "${yandex_lb_target_group.my_tg.id}"
+    healthcheck {
+      name = "tcp-check"
+      tcp_options {
+        port = 8080
+      }
+      interval = 2
+      timeout = 1
+      unhealthy_threshold = 2
+      healthy_threshold = 2
+    }
+  }
+
+}
+
+resource "yandex_lb_target_group" "my_tg" {
+  name      = "my-target-group"
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.foo.id}"
+    address   = "${yandex_compute_instance.catgpt-1.network_interface.0.ip_address}"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.foo.id}"
+    address   = "${yandex_compute_instance.catgpt-2.network_interface.0.ip_address}"
+  }
+}
 
